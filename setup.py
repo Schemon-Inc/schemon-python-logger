0515e596da07d3cf5108c621ad060c0ec216853e
kikashy <kikashy@hotmail.com>
2024-11-05 11:41:47 -0500

    Bump version to 0.0.4 and remove automatic git tag creation from setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,8 @@ from setuptools import setup, find_packages
 
 import subprocess
 
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 
-
-def create_git_tag(version):
-    try:
-        subprocess.check_call(["git", "tag", f"v{version}"])
-        subprocess.check_call(["git", "push", "origin", f"v{version}"])
-    except subprocess.CalledProcessError as e:
-        print(f"Error creating or pushing tag: {e}")
-
-
-create_git_tag(VERSION)
 
 setup(
     name="schemon-python-logger",
